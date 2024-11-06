381130d0db220bdc51e6dfb49ae9109cb7e6cefe
KM-drago <62215420+KM-drago@users.noreply.github.com>
2024-11-07 02:39:23 +0530

    Added NextJS

---
+++ src/app/page.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <a className="text-xl">Template</a>
+        <ol className="list-inside text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2">This is a template for Typescript fullstack development.</li>
+          <li className="mb-2">
+            Inspired by{" "}
+            <Link className="hover:underline" href={"https://create.t3.gg/"}>
+              T3-Stack →
+            </Link>
+            .
+          </li>
+        </ol>
+        <table className="w-full">
+          <tbody>
+            <tr>
+              <td>Frontend</td>
+              <td>
+                <Link className="hover:underline" href={"https://nextjs.org/"}>
+                  NextJS →
+                </Link>{" "}
+                (App Router)
+              </td>
+            </tr>
+            <tr>
+              <td>Backend</td>
+              <td>
+                <Link className="hover:underline" href={"https://trpc.io/"}>
+                  tRPC →
+                </Link>
+              </td>
+            </tr>
+            <tr>
+              <td>Database</td>
+              <td>
+                <Link className="hover:underline" href={"https://firebase.google.com/docs/firestore"}>
+                  Firestore →
+                </Link>
+              </td>
+            </tr>
+            <tr>
+              <td>Auth</td>
+              <td>
+                <Link className="hover:underline" href={"https://clerk.com/"}>
+                  Clerk →
+                </Link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a className="flex items-center gap-2 hover:underline hover:underline-offset-4" href="https://github.com/KM-drago" target="_blank" rel="noopener noreferrer">
+          <Image aria-hidden src="/github-mark-white.svg" alt="Globe icon" width={16} height={16} />
+          KM-drago
+        </a>
+      </footer>
+    </div>
+  );
+}
