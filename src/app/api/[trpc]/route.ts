63bda82dd5ca03783d0cb2dba5a79b561007fd0e
KM-drago <62215420+KM-drago@users.noreply.github.com>
2024-11-07 18:16:04 +0530

    Added tRPC

---
+++ src/app/api/[trpc]/route.ts
@@ -0,0 +1,27 @@
+import { appRouter } from "@/server/_app";
+import { createTRPCContext } from "@/server/trpc";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { NextRequest } from "next/server";
+
+const createContext = async (req: NextRequest) => {
+  return createTRPCContext({
+    headers: req.headers,
+  });
+};
+
+// API endpoints are at /api/<routerName>.<methodName>
+const handler = (req: NextRequest) =>
+  fetchRequestHandler({
+    endpoint: "/api",
+    req,
+    router: appRouter,
+    createContext: () => createContext(req),
+    onError:
+      process.env.NODE_ENV === "development"
+        ? ({ path, error }) => {
+            console.error(`❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`);
+          }
+        : undefined,
+  });
+
+export { handler as GET, handler as POST };
